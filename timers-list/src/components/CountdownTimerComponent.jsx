5130c7422788d291ce9f9af093e143fa0906be4d
mricom <mricomorillo@gmail.com>
2022-08-06 23:36:24 +0200

    Deleted decrease timer reducer completely.

--- timers-list/src/components/CountdownTimerComponent.jsx
+++ timers-list/src/components/CountdownTimerComponent.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { formatTimeString } from "../shared/utils";
 import {
-  decreaseTimeLeft,
   resetCountdownTimer,
   setCountdownTimer,
 } from "../redux/actions";
